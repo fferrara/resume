2f3ff5528d39c273806f7b3064daf8a1f1671f7a
Flavio Ferrara <thecrow.eric@gmail.com>
2017-03-18 17:17:11 -0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,174 @@
+var fs = require('fs');
+var path = require('path');
+
+var gulp = require('gulp');
+
+// Load all gulp plugins automatically
+// and attach them to the `plugins` object
+var plugins = require('gulp-load-plugins')();
+
+// Temporary solution until gulp 4
+// https://github.com/gulpjs/gulp/issues/355
+var runSequence = require('run-sequence');
+
+var pkg = require('./package.json');
+var dirs = pkg['h5bp-configs'].directories;
+
+var myPackage = require('./package.json');
+
+// ---------------------------------------------------------------------
+// | Helper tasks                                                      |
+// ---------------------------------------------------------------------
+
+gulp.task('archive:create_archive_dir', function () {
+    fs.mkdirSync(path.resolve(dirs.archive), '0755');
+});
+
+gulp.task('archive:zip', function (done) {
+
+    var archiveName = path.resolve(dirs.archive, pkg.name + '_v' + pkg.version + '.zip');
+    var archiver = require('archiver')('zip');
+    var files = require('glob').sync('**/*.*', {
+        'cwd': dirs.dist,
+        'dot': true // include hidden files
+    });
+    var output = fs.createWriteStream(archiveName);
+
+    archiver.on('error', function (error) {
+        done();
+        throw error;
+    });
+
+    output.on('close', done);
+
+    files.forEach(function (file) {
+
+        var filePath = path.resolve(dirs.dist, file);
+
+        // `archiver.bulk` does not maintain the file
+        // permissions, so we need to add files individually
+        archiver.append(fs.createReadStream(filePath), {
+            'name': file,
+            'mode': fs.statSync(filePath).mode
+        });
+
+    });
+
+    archiver.pipe(output);
+    archiver.finalize();
+
+});
+
+gulp.task('clean', function (done) {
+    require('del')([
+        dirs.archive,
+        dirs.dist
+    ]).then(function () {
+        done();
+    });
+});
+
+gulp.task('copy', [
+    'copy:.htaccess',
+    'copy:index.html',
+    'copy:license',
+    'copy:misc'
+]);
+
+gulp.task('copy:.htaccess', function () {
+    return gulp.src('node_modules/apache-server-configs/dist/.htaccess')
+        .pipe(plugins.replace(/# ErrorDocument/g, 'ErrorDocument'))
+        .pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('copy:index.html', function () {
+    return gulp.src(dirs.src + '/index.html')
+        .pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('copy:license', function () {
+    return gulp.src('LICENSE.txt')
+        .pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('sass', function (done) {
+    gulp.src(dirs.src + '/sass/app.scss')
+        .pipe(plugins.sass())
+        .on('error', plugins.sass.logError)
+        .pipe(gulp.dest(dirs.src + '/css'))
+        .pipe(plugins.minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(plugins.rename({extname: '.min.css'}))
+        .pipe(gulp.dest(dirs.src + '/css'))
+        .on('end', done);
+});
+
+gulp.task('css', ['sass'], function (done) {
+    gulp.src([
+        dirs.src + '/css/main.css',
+        'node_modules/normalize.css/normalize.css',
+        dirs.src + '/css/app.css'
+    ])
+        .pipe(plugins.concat('style.css'))
+        .pipe(plugins.minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(plugins.rename({suffix: '.' + myPackage.version}))
+        .pipe(gulp.dest(dirs.dist + '/css/'))
+        .on('end', done);
+});
+
+gulp.task('copy:misc', function () {
+    return gulp.src([
+
+        // Copy all files
+        dirs.src + '/**/*',
+
+        // Exclude the following files
+        // (other tasks will handle the copying of these files)
+        '!' + dirs.src + '/css/*.css',
+        '!' + dirs.src + '/sass',
+        '!' + dirs.src + '/sass/**',
+        '!' + dirs.src + '/index.html'
+
+    ], {
+
+        // Include hidden files by default
+        dot: true
+
+    }).pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('lint:js', function () {
+    return gulp.src([
+        'gulpfile.js',
+        dirs.src + '/js/*.js',
+        dirs.test + '/*.js'
+    ]).pipe(plugins.jscs())
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('jshint-stylish'))
+        .pipe(plugins.jshint.reporter('fail'));
+});
+
+
+// ---------------------------------------------------------------------
+// | Main tasks                                                        |
+// ---------------------------------------------------------------------
+
+gulp.task('archive', function (done) {
+    runSequence(
+        'build',
+        'archive:create_archive_dir',
+        'archive:zip',
+        done);
+});
+
+gulp.task('build', function (done) {
+    runSequence(
+        ['clean', 'lint:js', 'css'],
+        'copy',
+        done);
+});
+
+gulp.task('default', ['build']);
---
+++ src/js/conversation.js
@@ -0,0 +1,113 @@
+/**
+ * Created by flavio on 12/03/2017.
+ */
+
+var CONVERSATION;
+CONVERSATION = [
+    {
+        "message": "Hey there! How are you going? Welcome to my resume!"
+    },
+    {
+        "message": "What's that??",
+        "mine": true
+    },
+    {
+        "message": "The first-of-its-kind <strong>conversational resume</strong>."
+    },
+    {
+        "message": "An unusual resume, since I'm an unusual professional."
+    },
+    {
+        "message": "But don't worry, it's even more informative than the average resume."
+    },
+    {
+        "message": "Ok then, let's give it a try. Can you start from your <strong>education</strong>?",
+        "mine": true
+    },
+    {
+        "message": "Of course."
+    },
+    {
+        "message": "I got the <strong>bachelor's degree</strong> in Computer Engineering from the Politecnico di Milano. It was 2012."
+    },
+    {
+        "message": "Then, I spent 1 year at the Universidade Federal do Espirito Santo, in Vitória, Brazil."
+    },
+    {
+        "message": "Finally, I took my <strong>Master's degree</strong> (still Computer Engineering) in the Politecnico in 2015. FInal grade: 107/110."
+    },
+    {
+        "message": "Fine. What about your <strong>work experience</strong>?",
+        "mine": true
+    },
+    {
+        "message": "Still in 2015, I started working for Mogai/EBRAND at Vitória."
+    },
+    {
+        "message": "It was a cool R&D project called <a href=\"https://www.pic4turtle.com\">Pic4Turtle</a>, with the aim of classifying sea turtle species in pictures."
+    },
+    {
+        "message": "I tackled the problem using <strong>machine learning</strong> with optimal results (nearly 90% accuracy)."
+    },
+    {
+        "message": "Right after that, I became a full-stack developer for the whole project."
+    },
+    {
+        "message": "And now?",
+        "mine": true
+    },
+    {
+        "message": "Since mid 2016, I switched to a startup called <a href=\"https://www.kuau.com.br\">KUAU</a>, committed to <strong>radically change secundary education</strong> in Brazil."
+    },
+    {
+        "message": "My current job description is <strong>technical lead</strong>. In a small group such as ours, this means a bunch of tasks."
+    },
+    {
+        "message": "Full-stack developer, team leader and product manager."
+    },
+    {
+        "message": "What did you feel as the high points of these experiences?",
+        "mine": true
+    },
+    {
+        "message": "First of all, solving interesting and non-trivial problems without supervision. I love to solve problems with great results!"
+    },
+    {
+        "message": "Designing and building digital products, from the idea to the App Store. Working in a digital agency helps me to feed my creative side."
+    },
+    {
+        "message": "And leading a small team of young and smart devs to build a shared know-how."
+    },
+    {
+        "message": "Speaking of, I’m proud to report that I first introduce automated tests to them and, in 6 months, we went from zero to End-to-End testing!"
+    },
+    {
+        "message": "Sounds interesting. Can we do deeper into your <strong>tech skills</strong>?",
+        "mine": true
+    },
+    {
+        "message": "About that, since a picture is worth a thousand words:"
+    },
+    {
+        "message": ["Machine learning", "Python", "Javascript", "Java EE", "PHP/Zend", "TDD"],
+        "stars": [4.5, 4.5, 4.5, 4, 4, 4]
+    },
+    {
+        "message": " I'm a voracious learner too. Maybe we can talk about it later on."
+    },
+    {
+        "message": "Of course. Let's talk about yourself before. How would you describe <strong>your personality</strong>?",
+        "mine": true
+    },
+    {
+        "message": "In a word, <strong>eclectic</strong>."
+    },
+    {
+        "message": "There is my analytical self (<em>I’m a computer scientist, after all</em>). I’m reasonable, logical, assertive and dedicated."
+    },
+    {
+        "message": "Then we have my creative side, which makes me curious, intuitive, different and friendly."
+    },
+
+];
+
